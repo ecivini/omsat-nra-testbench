aece2157de29b453dab98cd7ceba01acbd0ec76c
ecivini <emanuele.civini@proton.me>
2025-06-03 23:08:05 +0200

    Update expected result of unknown problems based on successful result in benchmark file

--- scripts/extract_expected_problem_results.py
+++ scripts/extract_expected_problem_results.py
@@ -12,7 +12,7 @@ BENCHMARK_PATH_INDEX = 3
 BENCHMARK_RESULT_INDEX = 4
 
 expected_results = {}
-missing_result = []
+missing_or_unknown_result = []
 
 for base_path, dirs, files in os.walk(BENCHMARKS_PATH):
     for file in files:
@@ -24,13 +24,17 @@ for base_path, dirs, files in os.walk(BENCHMARKS_PATH):
             content = test_case.read()
             result = re.search(EXPECTED_RESULT_PATTERN, content)
             if not result:
-                missing_result.append(file)
+                missing_or_unknown_result.append(file)
                 print("No matches found for ", path)
                 continue
+            
+            result = result.group(1)
+            if result == "unknown":
+                missing_or_unknown_result.append(file)
 
-            expected_results[path] = result.group(1)
+            expected_results[path] = result
 
-if len(missing_result) > 0:
+if len(missing_or_unknown_result) > 0:
     results_from_benchmark = {}
     with open(LATEST_BENCHMARK, "r") as benchmark_file:
         for line in benchmark_file.readlines()[1:]:
@@ -38,7 +42,12 @@ if len(missing_result) > 0:
             
             path = fields[BENCHMARK_PATH_INDEX]
             result = fields[BENCHMARK_RESULT_INDEX]
-            if path not in expected_results and result in ["unsat", "sat"]:
+            useful_result_found = result in ["unsat", "sat"]
+
+            if useful_result_found and (
+                path not in expected_results 
+                or expected_results.get(path) == "unknown"
+            ):
                 print("Adding result from benchmark for ", path)
                 expected_results[path] = result
 
