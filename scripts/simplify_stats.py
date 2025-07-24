1a686b345accf4a430951ce493ed915c78a975ca
ecivini <emanuele.civini@proton.me>
2025-07-24 23:24:47 +0200

    Improving plots and stats report generation

--- scripts/simplify_stats.py
+++ scripts/simplify_stats.py
@@ -1,7 +1,7 @@
 import sys
+import json
 
 import matplotlib.pyplot as plt
-import numpy as np
 
 CSV_SEPARATOR = ","
 NUMBER_OF_FIELDS = 25
@@ -12,45 +12,59 @@ NANO_SUCCESSFUL_CHECK_CROSSES_INDEX = 21
 NANO_SUCCESSFUL_EPSILON_BOX_INDEX = 23
 NANO_SAT_INDEX = 4
 
+EXPECTED_RESULTS_PATH = "./data/expected_results.json"
+
 NANO_CSV_HEADER = "exact_sub,check_crosses,epsilon_box,benchmark\n"
 ONLY_CSV_HEAHER = "benchmark\n"
 BOTH_CSV_HEADER = "nano_time,vanilla_time,benchmark\n"
 
-def create_scatter_plot(nano_times: list, vanilla_times: list):
-    x = np.array(nano_times)
-    y = np.array(vanilla_times)
-    timeout = 10
+def get_problems_from_expected_result(result: str) -> dict:
+    expected_results = {}
+    with open(EXPECTED_RESULTS_PATH, "r") as expected_results_file:
+        expected_results = json.load(expected_results_file)
 
-    # Define tick positions (5 intermediate values)
-    tick_positions = np.linspace(0, timeout, 6)  # 6 ticks including 0 and 1000
+    problems = []
+    for problem, expected_result in expected_results.items():
+        if expected_result == result:
+            problems.append(problem)
+
+    return problems
+
+def create_scatter_plot(data: dict):
+    timeout = 1000
+    linthresh = 1  # Linear region from -1 to 1 (positive side only in this case)
 
     # Create figure
     fig, ax = plt.subplots(figsize=(7, 7))
 
     # Scatter plot
-    ax.scatter(x, y, color='orange', edgecolors='black', label="Data Points", s=80, zorder=3)
+    ax.scatter(data["no_error"]["x"], data["no_error"]["y"],
+               color='darkgreen', edgecolors='black', label="No errors", s=25, zorder=4, alpha=0.2)
+    ax.scatter(data["error_both"]["x"], data["error_both"]["y"],
+               color='yellow', edgecolors='black', label="Both errored", s=40, zorder=5)
 
     # Reference line y = x
-    ax.plot([0, timeout], [0, timeout], 'k--', label="y = x", zorder=2)
+    ax.plot([1e-2, timeout], [1e-2, timeout], 'k--', label="y = x", zorder=2)
 
     # Timeout lines (dashed)
     ax.axvline(timeout, linestyle='--', color='gray')
     ax.axhline(timeout, linestyle='--', color='gray')
 
-    # Set limits
-    ax.set_xlim(left=0, right=timeout * 1.1)
-    ax.set_ylim(bottom=0, top=timeout * 1.1)
+    # Set symlog scale
+    ax.set_xscale('symlog', linthresh=linthresh)
+    ax.set_yscale('symlog', linthresh=linthresh)
+    ax.set_aspect('equal')
 
-    # Set custom tick marks
-    ax.set_xticks(tick_positions)
-    ax.set_yticks(tick_positions)
+    # Set limits
+    ax.set_xlim(left=1e-2, right=timeout * 1.1)
+    ax.set_ylim(bottom=1e-2, top=timeout * 1.1)
 
     # Labels
     ax.set_xlabel('MathSAT + NaNo', fontsize=12)
     ax.set_ylabel('MathSAT Only', fontsize=12)
 
     # Grid
-    ax.grid(True, linestyle=":", linewidth=0.5)
+    ax.grid(True, which="both", linestyle=":", linewidth=0.5)
 
     # Legend
     ax.legend()
@@ -61,15 +75,13 @@ def create_scatter_plot(nano_times: list, vanilla_times: list):
 
 def process_vanilla_line(line):
     benchmark = line[TESTED_BENCHMARK_INDEX]
-    problem_sat = line[NANO_SAT_INDEX]
+    problem_result = line[NANO_SAT_INDEX]
     time = line[TIME_INDEX]
-
-    if problem_sat != "sat":
-        return None 
     
     return {
         "benchmark": benchmark,
-        "time": time
+        "time": time,
+        "result": problem_result
     }
 
 def process_nano_line(line):
@@ -77,70 +89,124 @@ def process_nano_line(line):
     succ_exact_sub = line[NANO_SUCCESSFUL_EXACT_SUB_INDEX]
     succ_check_crosses = line[NANO_SUCCESSFUL_CHECK_CROSSES_INDEX]
     succ_epsilon_box = line[NANO_SUCCESSFUL_EPSILON_BOX_INDEX]
+    problem_result = line[NANO_SAT_INDEX]
     time = line[TIME_INDEX]
     
-    at_least_one_nf = succ_exact_sub == "NF"
-    at_least_one_nf |= succ_check_crosses == "NF"
-    at_least_one_nf |= succ_epsilon_box == "NF"
-    if at_least_one_nf:
-        return None
+    # at_least_one_nf = succ_exact_sub == "NF"
+    # at_least_one_nf |= succ_check_crosses == "NF"
+    # at_least_one_nf |= succ_epsilon_box == "NF"
+    # if at_least_one_nf:
+    #     return None
 
-    if int(succ_exact_sub) + int(succ_check_crosses) + int(succ_epsilon_box) == 0:
-        return None
+    # if int(succ_exact_sub) + int(succ_check_crosses) + int(succ_epsilon_box) == 0:
+    #     return None
 
     return {
         "exact_sub": succ_exact_sub, 
         "check_crosses": succ_check_crosses,
         "epsilon_box": succ_epsilon_box,
         "benchmark": benchmark,
+        "result": problem_result,
         "time": time
     }
 
 def compute_solved_by_nano(nano_csv_path: str, vanilla_csv_path: str):
-    nano_sat_problems, nano_times = process_smt_csv(nano_csv_path, True)
+    nano_data = process_smt_csv(nano_csv_path, True)
     
     if vanilla_csv_path:
-        vanilla_sat_problems, vanilla_times = process_smt_csv(vanilla_csv_path, False)
+        vanilla_data = process_smt_csv(vanilla_csv_path, False)
 
         # Output problems solved by vanilla only
         vanilla_only_path = vanilla_csv_path[:-4] + "_only.csv"
         with open(vanilla_only_path, "w+") as vanilla_csv:
             vanilla_csv.write(ONLY_CSV_HEAHER)
-            for problem in vanilla_sat_problems:
-                if problem not in nano_sat_problems:
+            for problem in vanilla_data["sat"]:
+                if problem not in nano_data["sat"]:
                     line = problem + "\n"
                     vanilla_csv.write(line)
         
         # Output problems solved by both solvers
-        x_times = []
-        y_times = []
-
         both_short_path = vanilla_csv_path[:-4] + "_both.csv"
         with open(both_short_path, "w+") as both_csv:
             both_csv.write(BOTH_CSV_HEADER)
-            for problem in vanilla_sat_problems:
-                if problem in nano_sat_problems:
-                    line = nano_times[problem] + "," +\
-                        vanilla_times[problem] + "," +\
+            for problem in vanilla_data["sat"]:
+                if problem in nano_data["sat"]:
+                    line = nano_data["time"][problem] + "," +\
+                        vanilla_data["time"][problem] + "," +\
                         problem + "\n"
                     both_csv.write(line)
-
-                    x_times.append(float(nano_times[problem]))
-                    y_times.append(float(vanilla_times[problem]))
 
         # Output problems solved by nano only
         nano_only_path = nano_csv_path[:-4] + "_only.csv"
         with open(nano_only_path, "w+") as only_csv:
             only_csv.write(ONLY_CSV_HEAHER)
-            for problem in nano_sat_problems:
-                if problem not in vanilla_sat_problems:
+            for problem in nano_data["sat"]:
+                if problem not in vanilla_data["sat"]:
                     line = problem + "\n"
                     only_csv.write(line)
 
-        create_scatter_plot(x_times, y_times)
+        x_not_error_times = [] # NaNo
+        y_not_error_times = [] # Vanilla
+
+        x_nano_error_only_times = [] # NaNo
+        y_nano_error_only_times = [] # Vanilla
+
+        x_error_both_times = [] # NaNo
+        y_error_both_times = [] # Vanilla
+
+        problems_of_expected_result = get_problems_from_expected_result("unknown") # sat, unsat, unknown
+
+        counter = 0
+        all_problems = vanilla_data["sat"] + vanilla_data["timeout"] + vanilla_data["unsat"]
+        problems_of_interest = list(set(problems_of_expected_result) & set(all_problems))
+        for problem in problems_of_interest:
+            if problem in nano_data["error"]:
+                # x_nano_error_only_times.append(float(nano_data["time"][problem]))
+                # y_nano_error_only_times.append(float(vanilla_data["time"][problem]))
+                continue
+            else:
+                nano_time = float(nano_data["time"][problem])
+                nano_time = 1000.0 if nano_time >= 1000.0 or problem in nano_data["timeout"] else nano_time
+
+                vanilla_time = float(vanilla_data["time"][problem])
+                vanilla_time = 1000.0 if vanilla_time >= 1000.0 or problem in vanilla_data["timeout"] else vanilla_time
+
+                x_not_error_times.append(nano_time)
+                y_not_error_times.append(vanilla_time)
+
+                if nano_time < 1000.0 and vanilla_time >= 999.9:
+                    counter += 1
+                    #print(counter, vanilla_time, nano_time, problem)
+
+        for problem in problems_of_interest:
+            if problem in nano_data["error"]:
+                x_error_both_times.append(float(nano_data["time"][problem]))
+                y_error_both_times.append(float(vanilla_data["time"][problem]))
+            else:
+                #print("There should be no problems erroring only MathSAT vanilla and not NaNo.", problem)
+                pass 
+        data = {
+            "no_error": {
+                "x": x_not_error_times,
+                "y": y_not_error_times
+            },
+            "error_only_nano": {
+                "x": x_nano_error_only_times,
+                "y": y_nano_error_only_times
+            },
+            "error_both": {
+                "x": x_error_both_times,
+                "y": y_error_both_times
+            }
+        }
+
+        create_scatter_plot(data)
 
 def process_smt_csv(input_csv_path: str, is_nano: bool):
     sat_problems = []
+    unsat_problems = []
+    timeout_problems = []
+    error_problem = []
     solving_times = {}
 
     with open(input_csv_path, "r") as input_csv:
@@ -167,32 +233,35 @@ def process_smt_csv(input_csv_path: str, is_nano: bool):
             solving_time = None
             if is_nano:
                 stats = process_nano_line(line_vals)
-                if not stats:
-                    continue
+                if stats["result"] == "sat": 
+                    if stats["exact_sub"] != "NF" and int(stats["exact_sub"]) > 0:
+                        solved_by_exact_sub += 1
+                    elif stats["check_crosses"] != "NF" and int(stats["check_crosses"]) > 0:
+                        solved_by_check_crosses += 1
+                    elif stats["epsilon_box"] != "NF" and int( stats["epsilon_box"]) > 0:
+                        solved_by_epsilon_box += 1
 
-                if int(stats["exact_sub"]) > 0:
-                    solved_by_exact_sub += 1
-                elif int(stats["check_crosses"]) > 0:
-                    solved_by_check_crosses += 1
-                elif int( stats["epsilon_box"]) > 0:
-                    solved_by_epsilon_box += 1
-
-                out_line = stats["exact_sub"] + "," + stats["check_crosses"] + "," + \
-                    stats["epsilon_box"] + "," + stats["benchmark"] + "\n"
-                output_csv.write(out_line)
+                    out_line = stats["exact_sub"] + "," + stats["check_crosses"] + "," + \
+                        stats["epsilon_box"] + "," + stats["benchmark"] + "\n"
+                    output_csv.write(out_line)
 
                 benchmark = stats["benchmark"]
                 solving_time = stats["time"]
             else:
                 stats = process_vanilla_line(line_vals)
 
-                if not stats:
-                    continue
-
                 benchmark = stats["benchmark"]
                 solving_time = stats["time"]
             
-            sat_problems.append(benchmark)
+            if stats["result"] == "sat":
+                sat_problems.append(benchmark)
+            elif stats["result"] == "unsat":
+                unsat_problems.append(benchmark)
+            elif stats["result"] in ["timeout", "forced_timeout"]:
+                timeout_problems.append(benchmark)
+            else:
+                error_problem.append(benchmark)
+
             solving_times[benchmark] = solving_time
 
         if is_nano:
@@ -200,8 +269,15 @@ def process_smt_csv(input_csv_path: str, is_nano: bool):
             print("Problems solved via exact substitution: ", solved_by_exact_sub)
             print("Problems solved via check crosses:", solved_by_check_crosses)
             print("Problems solved via epsilon box:", solved_by_epsilon_box)
+            print("Problems solved by NaNo:", solved_by_exact_sub + solved_by_check_crosses + solved_by_epsilon_box)
 
-    return sat_problems, solving_times
+    return {
+        "sat": sat_problems, 
+        "unsat": unsat_problems, 
+        "timeout": timeout_problems, 
+        "error": error_problem, 
+        "time": solving_times
+    }
 
 def main():
     # Check cli args
@@ -221,7 +297,6 @@ def main():
         sys.exit(2)
 
     if vanilla_csv_file_path and not vanilla_csv_file_path.endswith(".csv"):
-        print(vanilla_csv_file_path.endswith(".csv"))
         print("Please provide a vanilla .csv file:", vanilla_csv_file_path)
         sys.exit(3)
 
